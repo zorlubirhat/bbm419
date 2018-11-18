652a7211e68d3bf09b5010eab0b7436a7acbbb1d
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-11-18 19:28:54 +0300

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,103 +1,149 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.Stack;
 
 public class Graph {
+    int vertex;
+    
+    private static int[][] M;
+    LinkedList<Integer> list[];
+    ArrayList<Integer> makerList = new ArrayList<>(); 
+    ArrayList<Integer> breakerList = new ArrayList<>(); 
+    Set<Integer> hs = new HashSet<>();
+    Set<Integer> hs1 = new HashSet<>();
+    
 
-	//Class arrays for Graph
-    private static int[][] B;               // 0 = no edge; 1 = red edge; -1 = blue edge
-    private static boolean [] visited;
-    
-    public Graph(int N){                            // a constructor for a instance of the class with N vertices 
-        B = new int[N][N];
-        visited = new boolean[B.length];
+    public Graph(int vertex) {
+        
+        M = new int[vertex][vertex];
+        this.vertex = vertex;
+        
+        list = new LinkedList[vertex];
+        for (int i = 0; i <vertex ; i++) {
+            list[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination, int w){
+
+        //add forward edge
+        list[source].addFirst(destination);
+       // list[destination].addFirst(source);
+        
+        this.M[source][destination] = w;
+        //this.M[destination][source] = w;
+        
+        
+        if(w==1) {
+            // Add v to u's list. 
+        		 
+            makerList.add(destination);
+           	hs.addAll(makerList);
+           	makerList.clear();
+           	makerList.addAll(hs);
+        }
+        if(w==(-1)) {
+        	breakerList.add(destination);
+        	hs1.addAll(breakerList);
+        	breakerList.clear();
+        	breakerList.addAll(hs1);
+        }
     }
     
-    public void addEdge(int u, int v, int w){       // add an edge from vertex u to v with value w (which in this hw will be  only 0, -1, or 1)
-        this.B[u][v] = w;
-        this.B[v][u] = w;
+    public void removeEdge(int source, int destination){         // remove the edge from u to v and the (duplicate) edge from v to u
+        list[source].remove(destination);
+        list[destination].remove(source);
+        
+    	this.M[source][destination] = 0;
+        this.M[destination][source] = 0;
     }
     
-    public void removeEdge(int u, int v){         // remove the edge from u to v and the (duplicate) edge from v to u
-        this.B[u][v] = 0;
-        this.B[v][u] = 0;
+    public int getEdge(int source, int destination){
+
+        return M[source][destination];
+
     }
     
-    public int getEdge(int u, int v){               // return the value (-1, 0, or 1) of the edge that goes from u to v
-        return this.B[u][v];
-    }
-    
-    public boolean isEdge(int u, int v){            // return true or false depending on whether there is an edge (of either color) from u to v
-        if(this.B[u][v] != 0){
+    public boolean isEdge(int source, int destination){            // return true or false depending on whether there is an edge from u to v
+
+        if(M[source][destination] != 0){
+        	
             return true;
+
         }else{
+
             return false;
+
         }
+
     }
-    
-    public int degree(int v){                       // return the number of edges of either color connected to vertex v
-        int counter = 0;
-        for(int i = 0; i < this.B[v].length; ++i){
-            if(this.B[v][i] != 0){
-                counter++;
-            }
-        }
-        return counter;
-    }
-    
-    public int degree(int v, int w){                // return the number of edges of color w connected to vertex v
-        int counter = 0;
-        for(int i = 0; i < this.B[v].length; ++i){
-            if(this.B[v][i] == w){
-                counter++;
-            }
-        }
-        return counter;
-    }
-    
-    public int sizeOfGraph(){
-        return B.length;
-    }
-    
-    public boolean isFull(){
-        for(int i = 0; i < B.length; i++){
-            if(degree(i) < B.length - 1)
-                return false;
-        }
-        return true;                
-    }
-    
-    public boolean isCycleOfLength(int n, int w){  // return true iff there is a cycle of length n among edges of color w 
-        for(int i = 0; i < B.length; i++){
-            visited[i] = true;
-            for(int j = 0; j < B.length; j++){
-                visited[j] = true;
-                if(getEdge(i, j) == w){
-                    if(isConnected(j, i, n, w))
-                        return true;
+
+    public void DFS(){
+        //System.out.print("Depth First Traversal: ");
+        boolean[] visited = new boolean[vertex];
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int startIndex=0; startIndex<vertex; startIndex++){
+            if(visited[startIndex]==false) {
+                stack.push(startIndex);
+                visited[startIndex] = true;
+                while (stack.isEmpty() == false) {
+                    int nodeIndex = stack.pop();
+                    //System.out.print(nodeIndex + " ");
+                    LinkedList<Integer> nodeList = list[nodeIndex];
+                    for (int i = 0; i < nodeList.size(); i++) {
+                        int dest = nodeList.get(i);
+                        if (getEdge(startIndex,dest)==1 && visited[dest] == false) {
+                        	stack.push(dest);
+                        	visited[dest] = true;
+                        }
+                        
+                    }
+                   
+                    
                 }
-                visited[j] = false;
+                
             }
-            visited[i] = false;
+            
         }
-        return false;
+        
+        System.out.println();
     }
-    
-    private boolean isConnected(int u, int v, int n, int w){
-        if(n == 2){
-            if(getEdge(u, v) == w)
-                return true;
-            else
-                return false;
-        }else{
-            for(int i = 0; i < B.length; i++){
-                if(getEdge(u, i) == w && !visited[i]){
-                    visited[i] = true;
-                    boolean temp = isConnected(i, v, --n, w);
-                    visited[i] = false;
-                    return temp;
+
+   /* public void printGraph(){
+        for (int i = 0; i <vertex ; i++) {
+            LinkedList<Integer> nodeList = list[i];
+            if(nodeList.isEmpty()==false) {
+                System.out.print("source = " + i + " is connected to nodes: ");
+                for (int j = 0; j < nodeList.size(); j++) {
+                    System.out.print(" " + nodeList.get(j));
                 }
             }
+            System.out.println();
         }
-        return false;
+    }*/
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(6);
+        graph.addEdge(0, 3,-1);
+        graph.addEdge(0, 2,1);
+        graph.addEdge(1, 2,1);
+        graph.addEdge(3, 1,1);
+        graph.addEdge(2, 5,1);
+        graph.addEdge(2, 0,1);
+        graph.addEdge(4, 3,1);
+        graph.addEdge(4, 1,-1);
+        graph.addEdge(5, 4, 1);
+        //graph.printGraph();
+        if(graph.makerList.size()==6) {
+        	
+        	graph.DFS();
+        	System.out.println("maker wins");
+        }
+        else {
+        	System.out.println("Breaker wins");
+        }
     }
-    
-   
 }
