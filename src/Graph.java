b2cd42bbfc5c75a716c58d1176233b156500ff83
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-11-29 01:49:21 +0300

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -32,8 +32,10 @@ class Graph
     void addEdge(int v,int u, int w) 
     { 
         adj[v].add(u);  // Add w to v's list. 
+        adj[u].add(v);
         
         this.M[v][u] = w;
+        this.M[u][v] = w;
         //this.M[destination][source] = w;
         
         
@@ -52,12 +54,60 @@ class Graph
         	breakerList.addAll(hs1);
         }
     } 
+    public void removeEdge(int v, int u){         // remove the edge from u to v and the (duplicate) edge from v to u
+        adj[v].remove(u);
+        adj[u].remove(v);
+    	
+    	this.M[u][v] = 0;
+        this.M[v][u] = 0;
+    }
     int getEdge(int v, int u) {
     	return M[v][u];
     }
+    
+    public boolean isEdge(int u, int v){            // return true or false depending on whether there is an edge (of either color) from u to v
+        if(this.M[u][v] != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public int degree(int v){                       // return the number of edges of either color connected to vertex v
+        int counter = 0;
+        for(int i = 0; i < this.M[v].length; ++i){
+            if(this.M[v][i] != 0){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    
+    public int degree(int v, int w){                // return the number of edges of color w connected to vertex v
+        int counter = 0;
+        for(int i = 0; i < this.M[v].length; ++i){
+            if(this.M[v][i] == w){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    
+    public int sizeOfGraph(){
+        return M.length;
+    }
+    
+    public boolean isFull(){
+        for(int i = 0; i < M.length; i++){
+            if(degree(i) < M.length - 1)
+                return false;
+        }
+        return true;                
+    }
+    
   
     // A function used by DFS 
-    void DFSUtil(int v,boolean visited[]) 
+    public boolean DFSUtil(int v,boolean visited[]) 
     { 
         // Mark the current node as visited and print it 
         visited[v] = true; 
@@ -72,37 +122,47 @@ class Graph
             	count++;
                 DFSUtil(n, visited); }
         } 
+        return false;
     } 
   
     // The function to do DFS traversal. It uses recursive DFSUtil() 
-    void DFS(int v) 
+    boolean DFS(int v) 
     { 
         // Mark all the vertices as not visited(set as 
         // false by default in java) 
         boolean visited[] = new boolean[V]; 
   
-        // Call the recursive helper function to print DFS traversal 
-        DFSUtil(v, visited); 
+        return DFSUtil(v, visited); 
     } 
   
     public static void main(String args[]) 
     { 
         Graph g = new Graph(6); 
+        Player p = new Player();
   
-        g.addEdge(0, 1,1); 
-        g.addEdge(2, 0,1); 
-        g.addEdge(1, 2,1); 
+        g.addEdge(0, 2,1); 
         g.addEdge(2, 0,-1); 
-        g.addEdge(2, 3,-1); 
+        g.addEdge(2, 1,1); 
+        g.addEdge(2, 0,-1); 
+        g.addEdge(2, 3,1); 
         g.addEdge(3, 4,1); 
         g.addEdge(4, 5,1);
-        g.addEdge(5, 3,1);
-        g.addEdge(5, 4,-1);
+        g.addEdge(5, 0,1);
+        g.addEdge(1, 4,1);
   
         System.out.println("Following is Depth First Traversal "+ 
-                           "(starting from vertex 0)"); 
+                           "(starting from vertex 0)");
+        /*if(g.makerList.size()==6) {
+        	//System.out.println(count);
+        	//p.chooseMove(g);
+        }
+        	
+        else {
+        	System.out.println("Breakers win");
+        }*/
         if(makerList.size()==6) {
         	g.DFS(0);
+        	//System.out.println(g.count);
         	if(count>=5) {
         		System.out.println("Maker wins");
         	}
