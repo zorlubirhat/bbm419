8436880b1cc26a95a520fb57257f72f8cba0aa6f
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-11-29 01:44:33 +0300

    Add files via upload

---
+++ src/Move.java
@@ -0,0 +1,10 @@
+class Move {
+    	  int source;
+    	  int target;
+    	  public Move(int s, int t) {
+    	    source = s; target = t; 
+    	  }
+    	  public String toString() {
+    	    return "(" + source + "," + target + ")";
+    	  }
+    	}  
