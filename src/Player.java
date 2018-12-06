1d78bff38bc531ea0bb5ea0ab2f5a23f2571a6bd
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-06 14:14:04 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,16 @@
     public class Player{
       //D - This is our class variable to hold the depth that the method will search to
-      private int D = 10;
+      private static int D = 10;
 
       //chooseMove - This method will utilize eval and minMax to decide what move
       // would be the best decision by the computer. It assesses different values for
       // every positibility of that the game could go to, and keep track of the best decision.
-      public Move chooseMove(Graph G) { 
+      public static Move chooseMove(Graph G) { 
         int max = -10000;
 
         Move best = new Move(0, 1);
-        for(int mmm = 0; mmm <7; mmm++){
-            for(int nnn = 0; nnn < 7; nnn++){
+        for(int mmm = 0; mmm < 5; mmm++){
+            for(int nnn = 0; nnn < 5; nnn++){
                 if(G.isEdge(mmm, nnn) == false && mmm != nnn){
                     best = new Move(mmm, nnn);
             }
@@ -20,31 +20,53 @@
         for(int i = 0; i < G.sizeOfGraph(); i++){
           for(int j = i+1; j < G.sizeOfGraph(); j++){
             if(i != j && G.isEdge(i, j) == false){
-              System.out.println(i + j + " are I and J");
-              G.addEdge(i, j, -1);
-              Move m = new Move(i, j);
-              int val = minMax( G, 1, 0, 0);
-              //System.out.println(val + " | " + i +  " " + j);
-              if(val >= max) {
-                            System.out.println("New best");
-                best = m;
-                max = val; 
-              }
-              G.removeEdge(i, j);
+            	/*System.out.println(i);
+            	System.out.println(j);*/
+            	/*System.out.print(i+" and ");
+            	System.out.print(j + " are I and J");*/
+            	G.addEdge(i, j, 1);
+            	Move m = new Move(i, j);
+            	int val = minMax( G, 1, 0, 0);
+            	//System.out.println(val + " | " + i +  " " + j);
+            	if(val >= max) {
+            		System.out.println("New best");
+            		best = m;
+            		max = val; 
+            	}
+            	G.removeEdge(i, j);
             }
           }
         }
-        System.out.println(best);
+        System.out.println("Best move: " +best);
         return best; 
       }
 
+      //eval - This method will assign values to each possibility within the tree and
+      // dictate which move would be the best for the computer to make.
+      private static int eval(Graph G){
+        int i = 0;
+        for(int j = 0; j < G.sizeOfGraph(); j++){
+          if(G.makerList.contains(j) && !G.breakerList.contains(j))
+            i += 1;
+          if(!G.makerList.contains(j) && G.breakerList.contains(j))
+            i += 6;
+        }
+        if(G.DFS(0)	== true) {
+        	i = 1000000000;}
+        else {
+        	i=-1000000000;
+        }
+          
+        
+          
+        return i;
+      }
 
       //minMax - This method will take in a couple of different parameters and construct
       // a tree with the different possibilities. This method will also preform alpha beta 
       // pruning to make the tree traversing more efficient.
-      int minMax(Graph G, int depth, int alpha, int beta ) {  
+      static int minMax(Graph G, int depth, int alpha, int beta ) {  
         if(depth == D || G.isFull())  
-        	// will upload eval func
           return eval(G); // stop searching and return eval  
         else if(depth%2 == 0) {  
           int val = -100000000;  
@@ -53,7 +75,7 @@
               if(!G.isEdge(i, j)){
                 alpha = Math.max(alpha, val); // update alpha with max so far  
                 if(beta < alpha) break; // terminate loop  
-                G.addEdge(i, j, -1);
+                G.addEdge(i, j, 1);
                 val = Math.max(val, minMax( G, depth+1, alpha, beta ));
                 G.removeEdge(i, j);
               }
@@ -67,7 +89,7 @@
               if(!G.isEdge(i, j)){
                 beta = Math.min(beta, val); // update beta with min so far  
                 if(beta < alpha) break; // terminate loop  
-                G.addEdge(i, j, 1);
+                G.addEdge(i, j, -1);
                 val = Math.min(val, minMax( G, depth+1, alpha, beta ) );
                 G.removeEdge(i, j);
               }
@@ -76,14 +98,11 @@
           return val;  
         } 
       }
+      /*public static void main(String args[]) {
+    	  Graph G = new Graph(6);
+    	  chooseMove(G);
+    	    
+      }*/
     }
-    class Move {
-    	  int source;
-    	  int target;
-    	  public Move(int s, int t) {
-    	    source = s; target = t; 
-    	  }
-    	  public String toString() {
-    	    return "(" + source + "," + target + ")";
-    	  }
-    	}
+    
+    
