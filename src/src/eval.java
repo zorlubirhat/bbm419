2b4e45b9af2f2cde2227119003f6ed0ff4322a19
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-07 01:43:25 +0300

    Update eval.java

--- src/src/eval.java
+++ src/src/eval.java
@@ -1,19 +1,26 @@
 
 public class eval {
+	
+	int evaluate(Graph G) {
+		int i=0;
+		i += eval(G);
+		return i;
+		
+	}
 	//eval - This method will assign values to each possibility within the tree and
     // dictate which move would be the best for the computer to make.
-	static int eval(Graph G){
+	int eval(Graph G){
         int i = 0;
         for(int j = 0; j < G.sizeOfGraph(); j++){
           if(G.makerList.contains(j) && !G.breakerList.contains(j))
-            i += 1;
+            i = 1;
           if(!G.makerList.contains(j) && G.breakerList.contains(j))
-            i += 6;
+            i = 6;
         }
         if(G.DFS(0)	== true) {
-        	i = 1000000000;}
+        	i = 10000;}
         else {
-        	i=-1000000000;
+        	i=-10000;
         }
           
         
