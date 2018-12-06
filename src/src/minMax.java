cc37c017c12bae4f614aee64317f6390ad9021b7
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-07 01:49:15 +0300

    Update minMax.java

--- src/src/minMax.java
+++ src/src/minMax.java
@@ -3,7 +3,7 @@ public class minMax {
 	
 	//D - This is our class variable to hold the depth that the method will search to
 	private static int D = 5;
-    static eval e = new eval();
+	static eval e = new eval();
     
     //minMax - This method will take in a couple of different parameters and construct
     // a tree with the different possibilities. This method will also preform alpha beta 
@@ -18,7 +18,7 @@ public class minMax {
 		// Terminating condition. i.e 
 		// leaf node is reached 
 		if(depth == D || G.isFull())  
-			return e.eval(G); 
+			return e.evaluate(G); 
 
 		if (maximizingPlayer) 
 		{ 
@@ -26,22 +26,20 @@ public class minMax {
 
 			// Recur for left and 
 			// right children 
-			 
-		for(int i = 0; i < G.sizeOfGraph(); i++){
-	            for(int j = i+1; j < G.sizeOfGraph(); j++){
-	              if(!G.isEdge(i, j)){
-		               
-		              alpha = Math.max(alpha, best); 
-
-		              // Alpha Beta Pruning 
-		              if (beta <= alpha) 
-		            	  break; 
-		              G.addEdge(i, j, 1);
-		              int val = minmax(G,depth + 1,false, alpha, beta);
-		              best = Math.max(best, val);
-		              G.removeEdge(i, j);
-		           } 
-		        }
+			for(int i = 0; i < G.sizeOfGraph(); i++){
+				for(int j = i+1; j < G.sizeOfGraph(); j++){
+					if(!G.isEdge(i, j)){
+						alpha = Math.max(alpha, best); 
+						
+						// Alpha Beta Pruning 
+						if (beta <= alpha) 
+							break; 
+						G.addEdge(i, j, 1);
+						int val = minmax(G,depth + 1,false, alpha, beta);
+						best = Math.max(best, val);
+						G.removeEdge(i, j);
+					} 
+				}
 			}
 			
 			
@@ -55,20 +53,20 @@ public class minMax {
 
 			// Recur for left and 
 			// right children 
-		for(int i = 0; i < G.sizeOfGraph(); i++){
-	            for(int j = i+1; j < G.sizeOfGraph(); j++){
-	              if(!G.isEdge(i, j)){
-			 
-	            	  beta = Math.min(beta, best); 
-	            	  // Alpha Beta Pruning 
-	            	  if (beta <= alpha) 
-	            		  break; 
-	            	  G.addEdge(i, j, 1);
-	            	  int val = minmax(G,depth + 1,true, alpha, beta);
-	            	  best = Math.min(best, val); 
-	            	  G.removeEdge(i, j);
-	              }
-	            }
+			for(int i = 0; i < G.sizeOfGraph(); i++){
+				for(int j = i+1; j < G.sizeOfGraph(); j++){
+					if(!G.isEdge(i, j)){
+						
+						beta = Math.min(beta, best); 
+						// Alpha Beta Pruning 
+						if (beta <= alpha) 
+							break; 
+						G.addEdge(i, j, 1);
+						int val = minmax(G,depth + 1,true, alpha, beta);
+						best = Math.min(best, val); 
+						G.removeEdge(i, j);
+					}
+				}
 			} 
 			return best; 
 		} 
