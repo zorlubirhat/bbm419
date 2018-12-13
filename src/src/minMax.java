ec4905f7fa1d2bf5b875f1d88bab9dd3702ef4be
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-14 02:32:31 +0300

    Update minMax.java

--- src/src/minMax.java
+++ src/src/minMax.java
@@ -1,37 +1,19 @@
-
 public class minMax {
 	
-	//D - This is our class variable to hold the depth that the method will search to
 	private static int D = 5;
 	static eval e = new eval();
-    
-    //minMax - This method will take in a couple of different parameters and construct
-    // a tree with the different possibilities. This method will also preform alpha beta 
-    // pruning to make the tree traversing more efficient.
-    static int MAX = 1000; 
-	static int MIN = -1000; 
+        static int MAX = 10000; 
+	static int MIN = -10000; 
 
-	// Returns optimal value for 
-	// current player (Initially called 
-	// for root and maximizer) 
 	int minmax(Graph G,int depth,Boolean maximizingPlayer, int alpha,int beta) { 
-		// Terminating condition. i.e 
-		// leaf node is reached 
 		if(depth == D || G.isFull())  
 			return e.evaluate(G); 
-
-		if (maximizingPlayer) 
-		{ 
+		if (maximizingPlayer){ 
 			int best = MIN; 
-
-			// Recur for left and 
-			// right children 
 			for(int i = 0; i < G.sizeOfGraph(); i++){
 				for(int j = i+1; j < G.sizeOfGraph(); j++){
 					if(!G.isEdge(i, j)){
 						alpha = Math.max(alpha, best); 
-						
-						// Alpha Beta Pruning 
 						if (beta <= alpha) 
 							break; 
 						G.addEdge(i, j, 1);
@@ -40,25 +22,16 @@ public class minMax {
 						G.removeEdge(i, j);
 					} 
 				}
-			}
-			
-			
-			
-			
+			}			
 			return best; 
 		} 
 		else
 		{ 
 			int best = MAX; 
-
-			// Recur for left and 
-			// right children 
 			for(int i = 0; i < G.sizeOfGraph(); i++){
 				for(int j = i+1; j < G.sizeOfGraph(); j++){
-					if(!G.isEdge(i, j)){
-						
+					if(!G.isEdge(i, j)){				
 						beta = Math.min(beta, best); 
-						// Alpha Beta Pruning 
 						if (beta <= alpha) 
 							break; 
 						G.addEdge(i, j, 1);
@@ -70,8 +43,5 @@ public class minMax {
 			} 
 			return best; 
 		} 
-
-
 	} 
-	
 }
