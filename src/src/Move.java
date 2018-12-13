937a1acb364eff3a6708d2a4080328e3f3996764
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-14 02:29:35 +0300

    Update Move.java

--- src/src/Move.java
+++ src/src/Move.java
@@ -1,10 +1,10 @@
 class Move {
-    	  int source;
-    	  int target;
-    	  public Move(int s, int t) {
-    	    source = s; target = t; 
-    	  }
-    	  public String toString() {
-    	    return "(" + source + "," + target + ")";
-    	  }
-    	}  
+	int source;
+	int target;
+	public Move(int s, int t) {
+		source = s; target = t; 
+	}
+	public String toString() {
+		return "(" + source + "," + target + ")";
+		}
+}  
