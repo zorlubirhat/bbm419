b5f571ff38d139208594a7eeb6304b17cf851d1d
bussracicekk <37375743+bussracicekk@users.noreply.github.com>
2018-12-25 15:44:09 +0300

    Update Player1.java

--- src/src/Player1.java
+++ src/src/Player1.java
@@ -5,56 +5,46 @@ import java.util.Set;
 
 public class Player1{
 	Graph g = new Graph(6);
+	static Player1 p = new Player1();
 	static minMax minMax = new minMax();
 	static int count=0;
-	 private static LinkedList<Integer> adj[];
-	 static ArrayList<Integer> makerList = new ArrayList<>();
-	 static Set<Integer> hs = new HashSet<>();
+	private static LinkedList<Integer> adj[];
+	static ArrayList<Integer> makerList = new ArrayList<>();
+	static Set<Integer> hs = new HashSet<>();
 	 
 	 
-	   public boolean connectedComponents(int w) { 
-	        // Mark all the vertices as not visited 
-	        boolean[] visited = new boolean[6]; 
-	        for(int v = 0; v < 6; ++v) { 
-	            if(!visited[v]) { 
-	                // print all reachable vertices 
-	                // from v 
-	                g.isConnectedSpanningTree(v,visited,1); 
-	                makerList.add(v);
-	                System.out.println(makerList); 
-	            } 
-	        }
-			return false; 
-	    } 
+	public boolean connectedComponents(int w) {
+		// Mark all the vertices as not visited 
+		boolean[] visited = new boolean[6]; 
+		for(int v = 0; v < 6; ++v) { 
+			if(!visited[v]) { 
+				// print all reachable vertices 
+				// from v 
+				if(g.isConnectedSpanningTree(v,visited,1)==false) {
+					makerList.add(v);}
+					System.out.println(makerList); 
+				} 
+			}
+		return false;
+	} 
 	public static Move chooseMove(Graph G) { 
-		//Player1 p = new Player1();
 		int max = -10000;
-		Move best = new Move(0,1);
+		Move best = new Move(5,4);
         for(int k = 5; k > 0; k--){
             for(int l = 5; l > 5; l--){
                 if(G.isEdge(k, l) == false && k != l) {
+                	p.connectedComponents(1);
                 	if(!makerList.contains(k) || !makerList.contains(l)) {
                 		best = new Move(k,l);
                 		
                 	}
                 }
-                //System.out.println("\n makerlist = " + makerList);
-                   	/*if(!makerList.contains(l)) {
-                   		if(G.isEdge(k, l) == false && k != l)
-                			best = new Move(k,l);
-                		/*else 
-                			best = new Move(k,l);
-                	}*/
-                
             }
         }
 
         for(int i = 0; i < G.sizeOfGraph(); i++){
           for(int j = i+1; j < G.sizeOfGraph(); j++){
             if(i != j && G.isEdge(i, j) == false){
-            	//best = new Move(i,j);
-            	/*System.out.println(i);
-           		System.out.println(j);*/
            		System.out.print(i+" and ");
            		System.out.print(j + " are I and J");
            		G.addEdge(i, j, 1);
